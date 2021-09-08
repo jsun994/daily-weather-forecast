c8304187e6dce745885a03804c239f7ca8c01791
jsun <jaysun054@gmail.com>
2021-09-07 20:31:26 -0700

    more javascript outline

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,20 @@
-var input = document.querySelector("#input");
-var search = document.querySelector("#search");
-var cityName = document.querySelector("#city-name");
+var userFormEl = document.querySelector("#user-form");
+var inputEl = document.querySelector("#input");
+var cityEl = document.querySelector("#city-name");
 
 var key = "bd049a07fbe50fe7d3fdc4b706ec6bdf";
 var storage = [];
 
-var inputHandler = function() {
-    var cityValue = input.value.trim();
+var inputHandler = function(event) {
+    event.preventDefault();
+
+    var cityValue = inputEl.value.trim();
 
     if (cityValue) {
         console.log(typeof(cityValue));
-        cityName.innerHTML = cityValue;
+        cityEl.innerHTML = cityValue;
     } else {
-        alert("Please enter a city");
+        alert("Please enter a city!");
     }
 };
 
@@ -32,4 +34,4 @@ var coordinates = function(city) {
     });
 };
 
-search.addEventListener("click", inputHandler);
+userFormEl.addEventListener("submit", inputHandler);
