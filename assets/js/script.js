728a070890dc1737d57b23fe1dd9e3db033bf7ed
jsun <jaysun054@gmail.com>
2021-09-08 11:38:42 -0700

    one call api

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,11 @@ var userFormEl = document.querySelector("#user-form");
 var inputEl = document.querySelector("#input");
 var cityEl = document.querySelector("#city-name");
 var recentEl = document.querySelector("#recent");
+var todayIcon = document.querySelector("#today-icon");
+var todayTemp = document.querySelector("#today-temp");
+var todayWind = document.querySelector("#today-wind");
+var todayHum = document.querySelector("#today-hum");
+var todayUV = document.querySelector("#today-uv");
 
 var key = "bd049a07fbe50fe7d3fdc4b706ec6bdf";
 var storage = [];
@@ -12,25 +17,59 @@ var inputHandler = function(event) {
     var cityValue = inputEl.value.trim();
 
     if (cityValue) {
-        //console.log(typeof(cityValue));
-        cityEl.innerHTML = cityValue;
         apiCity(cityValue);
     } else {
         alert("Please enter a city!");
     }
 };
 
-//get city
+//city api call
 var apiCity = function(city) {
     var weatherApi = "https://api.openweathermap.org/data/2.5/weather?q="
-    + city + "&APPID=" + key;
+    + city + "&units=imperial&APPID=" + key;
   
-    console.log(weatherApi);
+    //console.log(weatherApi);
+
     fetch(weatherApi).then(function(response) {
-        console.log(response);
+        //console.log(response);
         if (response.ok) {
             response.json().then(function(data) {
                 console.log(data);
+                var latitude = data.coord["lat"];
+                var longitude = data.coord["lon"];
+                
+                cityEl.innerHTML = data.name + " (" + moment().format("M-D-YYYY") + ")";
+                var icon = data.weather[0].icon;
+                todayIcon.setAttribute("src", "http://openweathermap.org/img/wn/" + icon + ".png");
+                
+                apiCurrent(latitude, longitude);
+            });
+        } else {
+            alert("Error: " + response.statusText);
+        }
+    });
+};
+
+//one call using lat & lon
+var apiCurrent = function(lat, lon) {
+    
+    var oneCall = "https://api.openweathermap.org/data/2.5/onecall?lat="
+    + lat + "&lon=" + lon +
+    "&exclude=minutely,hourly&units=imperial&APPID="
+    + key;
+
+    //console.log(oneCall);
+
+    fetch(oneCall).then(function(response) {
+        if (response.ok) {
+            response.json().then(function(data) {
+                
+                console.log(data);
+                todayTemp.innerHTML = data.current.temp;
+                todayWind.innerHTML = data.current.wind_speed + " ";
+                todayHum.innerHTML = data.current.humidity;
+                todayUV.innerHTML = data.current.uvi;
+
             });
         } else {
             alert("Error: " + response.statusText);
